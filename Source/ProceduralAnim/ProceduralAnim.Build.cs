fbe2a5d5e5530ff9bcf6b81afdec819a0ae7e5af
Eano <934154737@qq.com>
2026-02-24 02:58:27 +0800

    07 Optional Sphere trace example

--- Source/ProceduralAnim/ProceduralAnim.Build.cs
+++ Source/ProceduralAnim/ProceduralAnim.Build.cs
@@ -8,9 +8,13 @@ public class ProceduralAnim : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore",
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
